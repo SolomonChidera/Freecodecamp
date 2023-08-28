a212e7d488184cf33f683d04218761680333aafd
SolomonChidera <solochidera10@gmail.com>
2023-08-28 17:43:17 +0100

    #the basic of pointer: using the dereferencing operator without initializing a pointer

--- Pointer/001.c
+++ Pointer/001.c
@@ -5,5 +5,9 @@ int main(void)
 	int digit = 42;
 	printf("The address of digit = %p.\n", &digit);
 	printf("The value of digit using indirection operator = %d.\n", *(&digit));
+	int *addressOfDigit;
+	*addressOfDigit = &digit;
+	printf("Value of *addressOfDigit is the address of digit and here it is: %p.\n", addressOfDigit);
+
 	return (0);
 }
