ad28a74d79c7fb8be4313fc6e9e73edc0c2ff852
SolomonChidera <solochidera10@gmail.com>
2023-09-03 15:53:17 +0100

    pointer

--- Pointer/eg1.c
+++ Pointer/eg1.c
@@ -10,7 +10,11 @@ int main(void)
 	printf("The pointer 'ptr' is: %p\n", ptr);
 	printf("The address of num is: %p\n", &num);
 	/*printing the value of num*/
-	printf("The value of num is: %d\n",num);
+	printf("The value of num is: %d\n", num);
+	/*Dereferencing with pointer*/
+	*ptr = 600;
+	printf("The value of num is: %d\n", num);
+	printf("The value of num is: %d\n", *ptr);
 
 	return (0);
 }
